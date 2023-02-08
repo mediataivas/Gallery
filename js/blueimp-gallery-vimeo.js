ba98de61c3a5e6cc027deac5e4aa1bac4b802bfe
Tero Raisio <tero.raisio@vincit.fi>
2023-02-08 12:39:09 +0200

    Vimeourl test

--- js/blueimp-gallery-vimeo.js
+++ js/blueimp-gallery-vimeo.js
@@ -58,7 +58,7 @@ function factory($, Gallery) {
 
     loadAPI: function () {
       var that = this
-      var apiUrl = 'https://f.vimeocdn.com/js_opt/froogaloop2.min.js'
+      var apiUrl = 'https://player.vimeo.com/api/player.js'
       var scriptTags = document.getElementsByTagName('script')
       var i = scriptTags.length
       var scriptTag
